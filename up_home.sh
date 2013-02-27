016aac5830882ee3439f548f24c5ba8eaeb93bb8
liuyug <liuyug@yahoo.cn>
2013-02-27 10:06:16 +0800

    sync powerline.vim to ~/.vim/plugin/

--- up_home.sh
+++ up_home.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-#cd "$(dirname "${BASH_SOURCE}")"
-#git pull
+
+POWERLINE_PATH=$(python -c " 
+import os
+print ''.join([item for item in os.sys.path if 'Powerline' in item])
+")
+
+POWERLINE_VIM="$POWERLINE_PATH/powerline/bindings/vim/plugin/powerline.vim"
+
+
 function doIt() {
     rsync --exclude ".git/"         \
         --exclude ".gitignore"      \
         --exclude ".DS_Store"       \
         --exclude "goagent/"        \
         --exclude "bootstrap.sh"    \
+        --exclude "up_home.sh"      \
         --exclude "README.rst"      \
         -av                         \
-        . ~
+        . $HOME
+    if [ -f $POWERLINE_VIM ]; then
+        rsync -av $POWERLINE_VIM $HOME/.vim/plugin/powerline.vim
+    fi
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
 else
