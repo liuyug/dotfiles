18f4a2fd20cedd378fafd7ea1ac1ac1d8d10d911
liuyug <liuyug@yahoo.cn>
2013-06-04 12:11:37 +0800

    Check directory exists

--- modules/powerline.sh
+++ modules/powerline.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-git clone https://github.com/Lokaltog/powerline
+
+if [ -d "$((pwd))/powerline" ]; then
+    (cd powerline; git pull)
+else
+    git clone https://github.com/Lokaltog/powerline
+fi
 (cd powerline; sudo python setup.py install)
 cp powerline/font/PowerlineSymbols.otf $HOME/.fonts
 cp powerline/font/10-powerline-symbols.conf $HOME/.fonts.conf.d
