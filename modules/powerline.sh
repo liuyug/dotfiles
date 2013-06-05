48deb0a6437c5c93426bb699f374662958f137fb
liuyug <liuyug@yahoo.cn>
2013-06-05 16:51:53 +0800

    Add vim configuration

--- modules/powerline.sh
+++ modules/powerline.sh
@@ -11,3 +11,5 @@ cp powerline/font/PowerlineSymbols.otf $HOME/.fonts
 cp powerline/font/10-powerline-symbols.conf $HOME/.fonts.conf.d
 mkdir -p $HOME/.config
 cp -r powerline/powerline/config_files/ $HOME/.config/powerline/
+mkdir -p $HOME/.vim
+cp -r powerline/powerline/bindings/vim/plugin/ $HOME/.vim/
