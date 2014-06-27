a200165f451847b1651e6fd0e60265991d80adb0
liuyug <liuyug@gmail.com>
2014-06-27 09:15:24 +0800

    Add parameter

--- bin/rdp.sh
+++ bin/rdp.sh
@@ -1,12 +1,46 @@
 #!/bin/sh
 
-IP=$1
-rdesktop    \
-    -g workarea  \
-    -P  \
+usage()
+{
+    local prog=`basename $0`
+    echo "$prog <ip address> [user name]"
+    exit 0
+}
+
+
+if [ "x$1" = "x" ]; then
+    usage
+else
+    IP=$1
+fi
+
+if [ "x$2" = "x" ]; then
+    USER=Administrator
+else
+    USER="$2"
+fi
+
+rdesktop \
+    -u "$USER" \
+    -g workarea \
+    -P \
     -a 32 \
     -D \
     -x 0x80 \
-    -r disk:host=$HOME  \
-    -T "rdp - $IP"  \
+    -r disk:host=$HOME \
+    -T "rdp - $IP" \
     $IP &
+
+#    #define RDP5_DISABLE_NOTHING    0x00  
+#    #define RDP5_NO_WALLPAPER   0x01  
+#    #define RDP5_NO_FULLWINDOWDRAG  0x02  
+#    #define RDP5_NO_MENUANIMATIONS  0x04  
+#    #define RDP5_NO_THEMING     0x08  
+#    #define RDP5_NO_CURSOR_SHADOW   0x20  
+#    #define RDP5_NO_CURSORSETTINGS  0x40    /* disables cursor blinking */  
+#    #define RDP5_ENABLE_FONT_SMOOTHING 0x80  
+#
+#    0x8F # equals the modem default + font smoothing
+#    0x81 # equals the broadband default + font smoothing
+#    0x80 # equals the LAN default + font smoothing
+
--- bin/vpnssh.sh
+++ bin/vpnssh.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
+usage()
+{
+    local prog=`basename $0`
+    echo "$prog <user name> <host name>"
+    exit 0
+}
+
+if [[ "x$1" = "x" || "x$2" = "x" ]]; then
+    usage
+else
+    user=$1
+    host=$2
+fi
+
 # -f background
 # -p remote port
-ssh -qTnN -D 7070  proxy@xxx.com -p 22
+
+ssh -qTnN -D 7070 $user@$host -p 22
 
