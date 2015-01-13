762ffcfe18ee0f4f798c32792c0ea0744c720e1a
liuyug <liuyug@gmail.com>
2015-01-13 10:27:49 +0800

    Remove -D option

--- modules/cygwin_autosetup.cmd
+++ modules/cygwin_autosetup.cmd
@@ -3,18 +3,19 @@ rem cygwin download url: https://cygwin.com/setup-x86_64.exe
 set CYGWIN_SETUP=setup-x86_64.exe
 set CYGWIN_SITE=http://mirrors.163.com/cygwin/
 set CYGWIN_ROOT=C:\cygwin64
-set OPT=-q -D -O -s %CYGWIN_SITE%
+set OPT=-q -O -s %CYGWIN_SITE%
 
 rem packages
 rem utils
 set PKGS=unzip,tree,tmux,p7zip
 
 rem devel
-set PKGS=%PKGS%,git,gcc,make,ctags
-set PKGS=%PKGS%,python
+set PKGS=%PKGS%,git,gcc,make,ctags,sqlite3,gdb
+set PKGS=%PKGS%,python,python-openssl,python-setuptools,python-sip
 
 rem network
 set PKGS=%PKGS%,curl,wget,ping,netcat,whois,rsync,lftp
+set PKGS=%PKGS%,openldap-devel
 set PKGS=%PKGS%,penssh,openssl,openssl-devel
 
 echo on
