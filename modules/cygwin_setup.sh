144440a7091649d12b2f276817ff398ab854e27d
liuyug <liuyug@gmail.com>
2015-02-24 16:43:08 +0800

    remove beautifulsoup, fix pip install

--- modules/cygwin_setup.sh
+++ modules/cygwin_setup.sh
@@ -15,18 +15,20 @@ PKGS="unzip,tree,tmux,p7zip,ascii"
 
 # devel
 PKGS="$PKGS,git,gcc,make,ctags,sqlite3,gdb"
-PKGS="$PKGS,python,python-openssl,python-setuptools,python-six,python-pygments,python-numpy"
-PKGS="$PKGS,python3,python3-openssl,python3-setuptools,python3-six,python3-pygments,python3-numpy"
+PKGS="$PKGS,python,python-openssl,python-setuptools,python-six,python-pygments,python-numpy,python-beautifulsoup"
+PKGS="$PKGS,python3,python3-openssl,python3-setuptools,python3-six,python3-pygments,python3-numpy,python3-beautifulsoup"
 
 
 # network
 PKGS="$PKGS,curl,wget,ping,netcat,whois,rsync,lftp"
 PKGS="$PKGS,openldap,openldap-devel"
 PKGS="$PKGS,penssh,openssl,openssl-devel"
+PKGS="$PKGS,mkisofs"
 
 $CYGWIN_SETUP $OPT -P $PKGS
 
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-python3 get-pip.py
-
+if [ ! -f get-pip.py ]; then
+    wget -N https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+    python get-pip.py
+fi
