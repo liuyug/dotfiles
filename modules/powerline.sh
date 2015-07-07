de3c4dace28abe4466997ee461a0b74e750e1ed7
liuyug <liuyug@gmail.com>
2015-07-07 12:32:55 +0800

    Fix powerline font

--- modules/powerline.sh
+++ modules/powerline.sh
@@ -7,9 +7,11 @@ fi
 
 (cd powerline; git clone https://github.com/Lokaltog/powerline)
 (cd powerline/powerline; python setup.py install --user)
-(cd powerline; git clone https://github.com/powerline/fonts)
-(cd powerline/fonts; bash install.sh)
+(cd powerline/powerline/font; cp PowerlineSymbols.otf $HOME/.fonts)
+# (cd powerline; git clone https://github.com/powerline/fonts)
+# (cd powerline/fonts; bash install.sh)
 
+fc-cache -vf
 
 # powerline config files
 mkdir -p $HOME/.config/powerline
