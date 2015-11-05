418d33a390d3487e0b09ce4c90653dab14c29c0d
liuyug <liuyug@gmail.com>
2015-11-05 10:20:16 +0800

    Fix bug

--- bin/kodi_u.sh
+++ bin/kodi_u.sh
@@ -3,6 +3,6 @@
 # force use ALSA, default PLUSE
 # AE_SINK=ALSA
 
-su - kodi -c "xinit kodi-standalone -- :0"
+sudo -u kodi /usr/bin/xinit /usr/bin/kodi-standalone -- :0
 
 # vim: tabstop=4 shiftwidth=4 expandtab
