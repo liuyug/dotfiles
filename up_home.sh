5f6e4fe35899abd47348abf4ac01e99a69d40806
root <root@localhost.localdomain>
2016-02-14 01:59:55 -0800

    Fix font function

--- up_home.sh
+++ up_home.sh
@@ -11,11 +11,31 @@ function vim_init()
     vim +PluginInstall +qall
 }
 
-function mono_font()
+function get_font()
 {
-    if [ ! -f $HOME/.fonts/Inconsolata.otf ]; then
+    fonts=(
+        [powerline]=https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+        [inconsolata]=http://www.levien.com/type/myfonts/Inconsolata.otf
+    )
+
+    url=${fonts[$1]}
+    file=`basename $url`
+
+    echo "Get font $file from $url"
+
+    if [ ! -f $HOME/.fonts/$file ]; then
         mkdir -p $HOME/.fonts
-        (cd $HOME/.fonts; wget http://www.levien.com/type/myfonts/Inconsolata.otf)
+        (cd $HOME/.fonts; wget $url)
+    fi
+}
+
+function powerline_font()
+{
+    url = https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+    file = `basename $url`
+    if [ ! -f $HOME/.fonts/$file ]; then
+        mkdir -p $HOME/.fonts
+        (cd $HOME/.fonts; wget $url)
     fi
 }
 
@@ -38,10 +58,11 @@ else
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         doIt
     fi
-    read -p "Download mono font to \"\$HOME/.fonts\". Are you sure? (y/n) " -n 1
+    read -p "Download Inconsolata and POWERLINE font to \"\$HOME/.fonts\". Are you sure? (y/n) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        mono_font
+        get_font inconsolata
+        get_font powerline
     fi
 
 fi
