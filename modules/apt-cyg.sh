6f69b515f27d13333cfe7346383a43382d2a2f8c
liuyug <liuyug@gmail.com>
2016-10-10 10:03:58 +0800

    download cygwin setup.exe

--- modules/apt-cyg.sh
+++ modules/apt-cyg.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+dest_dir="$HOME/.local/bin"
+
+cyg_setup_url="https://cygwin.com/setup-x86_64.exe"
+cyg_setup="$dest_dir/setup-x86_64.exe"
+
 url="https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg"
 target="$HOME/.local/bin/apt-cyg"
+
+echo "Download 'cygwin setup' from $cyg_setup_url"
+curl $cyg_setup_url -o $cyg_setup
 
 echo "Download 'apt-cyg' from $url"
 curl $url -o $target
