b95d3d374c6802a29ab4f0cc9c7b3f9bdad4b937
liuyug <liuyug@gmail.com>
2016-10-24 14:17:43 +0800

    fix ubuntu

--- modules/ubuntu_postinstall.sh
+++ modules/ubuntu_postinstall.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 
-echo "Need root privilege to run"
+codename=`lsb_release -c|cut -f2`
 
+lsb_release -a
 
 cat <<EOF >> /etc/apt/sources.list
-deb http://mirrors.163.com/ubuntu/ trusty main restricted
-deb http://mirrors.163.com/ubuntu/ trusty-update main restricted
-deb http://mirrors.163.com/ubuntu/ trusty-security main restricted
-
-deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted
-deb http://mirrors.aliyun.com/ubuntu/ trusty-update main restricted
-deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted
+deb http://cn.archive.ubuntu.com/ubuntu/ $codename main restricted universe
+deb http://cn.archive.ubuntu.com/ubuntu/ $codename-updates main restricted universe
+# deb http://cn.archive.ubuntu.com/ubuntu/ $codename-backports main restricted universe
+deb http://security.ubuntu.com/ubuntu/ $codename-security main restricted universe
 EOF
-apt-get update
 
-normal="vim vim-gui-common git curl lynx wget ssh tmux python-pip rsync"
+apt-get update
+apt-get upgrade
+
+normal="sudo vim vim-gui-common git curl lynx wget ssh tmux python-pip rsync"
+vmware="open-vmware-tools"
 dev="build-essential cmake"
 
 
-for item in `echo $normal`; do
+for item in `echo $normal $vmware $dev`; do
     echo "Install $item"
     apt-get install $item
 done
-
-
 
 # vim: ts=4 sw=4 et tw=79
