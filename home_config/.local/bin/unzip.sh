60b0cec92f25a75ac1d88a8799a98f20432a8663
LIU Yugang <liuyug@gmail.com>
2017-02-23 12:03:38 +0800

    fix unzip.sh

--- home_config/.local/bin/unzip.sh
+++ home_config/.local/bin/unzip.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-LANG=C /usr/bin/7za x -y "$1" | sed -n 's/^Extracting //p' | sed '1!G;h;$!d' | xargs convmv -f gbk -t utf8 --notest >/dev/null 2>/dev/null
+# LANG=C /usr/bin/7za x -y "$1" | sed -n 's/^Extracting //p' | sed '1!G;h;$!d' | xargs convmv -f gbk -t utf8 --notest >/dev/null 2>/dev/null
+
+
+if [ "x$1" = "x" ]; then
+    echo "unzip.sh <zip file>"
+    exit 0
+fi
+
+LANG=C /usr/bin/7za x -y "$1"
+
+echo "Please run \"convmv -f gbk -t utf8 --notest -r .\""
