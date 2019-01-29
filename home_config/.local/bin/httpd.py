4cdcf130a0c29c1ed196d1efe3727e9957f4c731
LIU Yugang <liuyug@gmail.com>
2019-01-29 13:47:06 +0800

    fix

--- home_config/.local/bin/httpd.py
+++ home_config/.local/bin/httpd.py
@@ -17,8 +17,8 @@ class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
 
 def run():
     parser = argparse.ArgumentParser(description='Simple HTTP(s) Server with threading')
-    parser.add_argument('--cert', help='HTTPs cert file')
-    parser.add_argument('--key', help='HTTPs key file')
+    parser.add_argument('--cert', help='HTTPs cert file. To generate new cert/key: "openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 3650"')
+    parser.add_argument('--key', help='HTTPs key file. To generate new cert/key: see above')
     parser.add_argument('listen', nargs='?', default='127.0.0.1:8000',
                         help='Listen IPAddress and Port. default: 127.0.0.1:8000')
     args = parser.parse_args()
