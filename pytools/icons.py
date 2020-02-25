227cf1a0ba742c41e0426c86bab0871157bb9348
LIU Yugang <liuyug@gmail.com>
2020-02-25 11:06:14 +0800

    fix for qt5

--- pytools/icons.py
+++ pytools/icons.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-
+import os
 import signal
 import sys
 from types import MethodType
 
-from PyQt4.QtCore import QSignalMapper, Qt, QTimer
-from PyQt4.QtGui import QAbstractItemView, QAction, QApplication, QIcon, QKeySequence, QLineEdit, QMainWindow, QSortFilterProxyModel, QStandardItem, QStandardItemModel, QTableView, QToolBar
+from PyQt5.QtCore import QSignalMapper, Qt, QTimer, QSortFilterProxyModel
+from PyQt5.QtWidgets import QAbstractItemView, QAction, QApplication, QLineEdit, QMainWindow, QTableView, QToolBar
+from PyQt5.QtGui import QIcon, QKeySequence, QStandardItem, QStandardItemModel
+
+from meditor import qrc_icon_theme
+
 
 def main(icon_spec):
     app = QApplication(sys.argv)
@@ -39,6 +43,9 @@ def main(icon_spec):
     proxy_model.setSourceModel(item_model)
 
     # get all icons and their available sizes
+    # QIcon.setThemeName('Tango')
+    QIcon.setThemeName('embed_qrc')
+
     icons = []
     all_sizes = set([])
     for context, icon_names in icon_spec:
@@ -70,7 +77,7 @@ def main(icon_spec):
     filter_line_edit.textChanged.connect(filter_changed)
     tool_bar.addWidget(filter_line_edit)
 
-    # actions to toggle visibility of available sizes/columns 
+    # actions to toggle visibility of available sizes/columns
     def action_toggled(index):
         column = 2 + index
         table_view.setColumnHidden(column, not table_view.isColumnHidden(column))
@@ -164,6 +171,7 @@ def main(icon_spec):
 
     main_window.showMaximized()
     return app.exec_()
+
 
 # icon naming specification http://standards.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
 icon_spec = []
@@ -383,13 +391,12 @@ icon_spec.append(('emotions', [
     'face-wink',
     'face-worried',
 ]))
-#is_3166_1_alpha_2 = ['ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bo', 'bq', 'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'ir', 'is', 'it', 'je', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mf', 'mg', 'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'ss', 'st', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'um', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 'za', 'zm', 'zw']
-#flags = []
-#for code in is_3166_1_alpha_2:
-#    flags.append('flag-%s' % code)
-#icon_spec.append(('intl', flags))
+# is_3166_1_alpha_2 = ['ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bo', 'bq', 'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'ir', 'is', 'it', 'je', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mf', 'mg', 'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'ss', 'st', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'um', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 'za', 'zm', 'zw']
+# flags = []
+# for code in is_3166_1_alpha_2:
+#     flags.append('flag-%s' % code)
+# icon_spec.append(('intl', flags))
 icon_spec.append(('mimetypes', [
-    # 
     'application-x-executable',
     'audio-x-generic',
     'font-x-generic',
@@ -479,9 +486,5 @@ icon_spec.append(('status', [
     'weather-storm',
 ]))
 
-
-def run():
-    sys.exit(main(icon_spec))
-
 if __name__ == '__main__':
-    run()
+    sys.exit(main(icon_spec))
