3f7b245cdc701f650d4a421073a524e6b1aff867
Yugang LIU <liuyug@gmail.com>
2020-06-18 15:08:39 +0800

    make dmg

--- bin/make_macos.sh
+++ bin/make_macos.sh
@@ -4,6 +4,28 @@ Catalina_size=9000m
 Mojave_size=6000m
 High_Sierra_size=5500m
 
+function app_to_dmg()
+{
+    app_label=`basename $mac_app .app`
+    os_name=`echo $app_label | cut -d" " -f 3`
+    size_0=${os_name}_size
+    size=${!size_0}
+    dmg_name="temp_dmg"
+
+    sudo hdiutil create -o ${dmg_name}.dmg \
+        -size $size -layout SPUD -fs HFS+J
+    mkdir $dmg_name
+    sudo hdiutil attach ${dmg_name}.dmg \
+        -noverify -mountpoint $dmg_name
+    sudo "$mac_app/Contents/Resources/createinstallmedia" \
+        --volume $dmg_name
+
+    sudo hdiutil detach "/Volumes/${app_label}"
+    rmdir $dmg_name
+    sudo mv ${dmg_name}.dmg $output/${app_label}.dmg
+    echo "You can convert dmg to iso by use 'dmg2img <dmg> <iso>' or 'sudo hdiutil convert -format UDTO -o <iso>  <dmg>'"
+}
+
 function app_to_iso()
 {
 
@@ -41,7 +63,7 @@ output="$3"
 IFS=$'\t\n'
 
 if [ "x$cmd" = "x" ] ; then
-    echo "$0 <usb|iso> <mac app path> <output path>"
+    echo "$0 <usb|iso|dmg> <mac app path> <output path>"
     exit 0
 fi
 
