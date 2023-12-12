4a5fcf0a6e06406a199b7e5ad1d6fa9263641aeb
Yugang LIU <liuyug@gmail.com>
2023-12-12 15:02:31 +0800

    fix

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -2,48 +2,30 @@
 # -*- encoding:utf-8 -*-
 
 import argparse
-import logging
-
-logger = logging.getLogger(__name__)
 
 
-class App():
+class App(object):
     name = 'Hello'
-    description = 'Hello'
+    description = ''
     version = '1.0'
     url = ''
     author_email = 'author@gmail.com'
-    license = 'MIT'
+    license = ''
 
-    @classmethod
-    def run(cls):
+    def run(self):
         about = f'{App.name} v{App.version} {App.description}'
         parser = argparse.ArgumentParser(description=App.description)
         parser.add_argument('--version', action='version', version=about,
                             help='show version')
-        parser.add_argument('-v', '--verbose', action='count',
-                            default=0, help='verbose output')
 
-        parser.add_argument('--hello', help='hello')
+        parser.add_argument('--hello', action='store_true', help='hello')
 
         args = parser.parse_args()
-        level = logging.INFO - args.verbose * 10
-        if level == logging.DEBUG:
-            logging.basicConfig(
-                level=level,
-                format='%(levelname)s:%(name)s:%(message)s',
-            )
-        else:
-            logging.basicConfig(
-                level=level,
-                format='%(levelname)s:%(message)s',
-            )
-
         if args.hello:
-            print('hello %s' % args.hello)
+            print('hello')
         else:
             parser.print_help()
 
 
 if __name__ == '__main__':
-    App.run()
+    App().run()
