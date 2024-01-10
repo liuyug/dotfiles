1cba228aebab953386407185c714275d6051ef7f
Yugang LIU <liuyug@gmail.com>
2024-01-11 05:37:12 +0800

    fix

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -3,6 +3,12 @@
 
 import argparse
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+logger = logging.getLogger(__name__)
+
 
 class App(object):
     name = 'Hello'
@@ -17,10 +23,34 @@ class App(object):
         parser = argparse.ArgumentParser(description=App.description)
         parser.add_argument('--version', action='version', version=about,
                             help='show version')
+        parser.add_argument('-v', '--verbose', action='count',
+                            default=0, help='verbose output')
 
         parser.add_argument('--hello', action='store_true', help='hello')
 
         args = parser.parse_args()
+
+        if args.verbose > 1:
+            level = logging.INFO - args.verbose * 10
+            logging.basicConfig(
+                level=level,
+                format="[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]%(levelname)s: %(message)s",
+            )
+        else:
+            # handlers = [
+            #     RotatingFileHandler(
+            #         filename='app.log',
+            #         mode='w',
+            #         maxBytes=512000,
+            #         backupCount=4
+            #     )
+            # ]
+            logging.basicConfig(
+                # handlers=handlers,
+                level=logging.INFO,
+                format='%(levelname)s:%(message)s',
+            )
+
         if args.hello:
             print('hello')
         else:
