9fe0ea10fcfeac9241a1bad6df97b21ad5c9ca8a
liuyug <liuyug@gmail.com>
2026-01-03 07:30:32 +0800

    fix

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- usage:
+-- ctrl + q " search result to quickfix list
+-- :cfdo %s/{find}/{replace}/g | update " replace and save
 
 
 require("FTerm").setup({
